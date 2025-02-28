fa19fa58011331a732248fea43d468d734cee82b
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-02-28 20:17:17 +0000

    WIP change implementation to use classes

--- main.js
+++ main.js
@@ -1,52 +1,75 @@
-const myLibrary = [
-    { title: '"The Catcher in the Rye"', author: 'J.D. Salinger', pages: 224, read: true },
-
-    { title: '"The Great Gatsby"', author: 'F. Scott Fitzgerald', pages: 180, read: false },
-
-    { title: '"To Kill a Mockingbird"', author: 'Harper Lee', pages: 336, read: false }
-];
-const emptyDisplayMessage = document.querySelector('.empty-display-message');
-const cardsContainer = document.querySelector('.cards-container');
-const form = document.querySelector('#add-book-form');
-const submitButton = document.querySelector('#submit-button');
-const formTitle = document.querySelector('#form-title')
-const formAuthor = document.querySelector('#form-author')
-const formPages = document.querySelector('#form-pages')
-const readYes = document.querySelector('#read-yes')
-const readNo = document.querySelector('#read-no')
-
-function formSubmition() {
-    addBookToLibrary (formTitle.value, formAuthor.value, formPages.value, formTitle.value);
-    formTitle.value = null;
-    formAuthor.value = null;
-    formPages.value = null;
-    formTitle.value = null;
-};
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        if (read === true) {
-            return `The ${title} by ${author}, ${pages} pages, already read`
-        } else {
-            return `The ${title} by ${author}, ${pages} pages, not read yet`
+class Book {
+    constructor(title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+        this.info = function() {
+            if (status === true) {
+                return `The ${title} by ${author}, ${pages} pages, already read`
+            } else {
+                return `The ${title} by ${author}, ${pages} pages, not read yet`
+            };
         };
-    };
+    }
 };
 
-function addBookToLibrary (userTitle, userAuthor, userPages, userRead) {
-    const addedBook = new Book(userTitle, userAuthor, userPages, userRead);
-    myLibrary.unshift(addedBook);
-    displayBooks ();
-};
+class Library {
+    constructor() {
+        this.books = [
+            { title: '"The Catcher in the Rye"', author: 'J.D. Salinger', pages: 224, status: "read" },
+            { title: '"The Great Gatsby"', author: 'F. Scott Fitzgerald', pages: 180, status: "not read yet" },
+            { title: '"To Kill a Mockingbird"', author: 'Harper Lee', pages: 336, status: "currently reading" }
+        ];
+    }
 
-function removeAllCards () {
-    while (cardsContainer.lastElementChild) {
-        cardsContainer.removeChild(cardsContainer.lastElementChild);
-      }
+    addBook(title, author, pages, status) {
+        const book = new Book(title, author, pages, status);
+        this.books.push(book);
+    }
+
+    removeBook(title) {
+        this.books = this.books.filter(book => book.title !== title);
+    }
+}
+
+class ScreenController {
+    constructor(library) {
+        this.library = library;
+        this.getPageElements();
+    }
+
+    getPageElements() {
+        this.emptyDisplayMessage = document.querySelector('.empty-display-message');
+        this.cardsContainer = document.querySelector('.cards-container');
+        this.form = document.querySelector('#add-book-form');
+        this.submitButton = document.querySelector('#submit-button');
+        this.formTitle = document.querySelector('#form-title');
+        this.formAuthor = document.querySelector('#form-author');
+        this.formPages = document.querySelector('#form-pages');
+        this.formStatus = document.querySelector('#select');
+    }
+
+    formSubmission() {
+        const title = this.formTitle.value;
+        const author = this.formAuthor.value;
+        const pages = this.formPages.value;
+        const status = this.formStatus.value;
+
+        this.library.addBook(title, author, pages, status);
+        this.formTitle.value = "";
+        this.formAuthor.value = "";
+        this.formPages.value = "";
+        this.formStatus.value = "";
+    }
+
+    // add displayBooks() function here
+
+    removeAllCards() {
+        while (this.cardsContainer.lastChild) {
+            this.cardsContainer.removeChild(this.cardsContainer.lastElementChild);
+        }
+    }
 }
 
 function displayBooks () {
@@ -57,21 +80,21 @@ function displayBooks () {
         const paraTitle = document.createElement('p');
         const paraAuthor = document.createElement('p');
         const paraPages = document.createElement('p');
-        const paraRead = document.createElement('p');
+        const paraStatus = document.createElement('p');
 
         paraTitle.textContent = element.title;
         paraAuthor.textContent = element.author;
         paraPages.textContent = `${element.pages} pages`;
-        if (element.read === true) {
-            paraRead.textContent = 'Read';
-        } else if (element.read === false) {
-            paraRead.textContent = 'Not read yet';
+        if (element.status === true) {
+            paraStatus.textContent = 'Read';
+        } else if (element.status === false) {
+            paraStatus.textContent = 'Not read yet';
         }
 
         card.appendChild(paraTitle);
         card.appendChild(paraAuthor);
         card.appendChild(paraPages);
-        card.appendChild(paraRead);
+        card.appendChild(paraStatus);
         cardsContainer.appendChild(card);
     }
     if (myLibrary[0] !== undefined) {
@@ -81,5 +104,5 @@ function displayBooks () {
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    formSubmition();
+    formSubmission();
 });
